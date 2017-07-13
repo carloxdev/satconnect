5d46d56cb9829896a9f1e234b43ad348054a1721
Carlos <carloxdev@gmail.com>
2017-07-13 12:36:31 -0500

    Afinando desarrollo

--- monitor_cxp.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-NAME="monitor_cxp"
-
-echo "Starting $NAME"
-cd /webapps/satconnect/src/
-exec  /webapps/smartcfdi/envlnx/bin/python monitor_cxp.py
--- monitor_sat.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-NAME="monitor_sat"
-
-echo "Starting $NAME"
-cd /webapps/satconnect/src/
-exec  /webapps/smartcfdi/envlnx/bin/python monitor_sat.py
