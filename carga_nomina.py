a62868767d70000d7d04c1cadbe2ed4eb80b0150
Jorgemh <=>
2017-09-27 12:51:08 -0500

    Carga masiva XML Nomina

---
+++ carga_nomina.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from LibTools.filesystem import Carpeta
+from slaves import SentinelNomina
+
+import settings
+
+if __name__ == '__main__':
+
+    carpeta = Carpeta(settings.folder_nomina)
+    sentinela = SentinelNomina(carpeta)
+    sentinela.file_upload()
